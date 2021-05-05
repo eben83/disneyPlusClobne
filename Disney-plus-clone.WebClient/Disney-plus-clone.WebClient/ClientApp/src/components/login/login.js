c34b80dd1237001a34342fbb8193462e9f0c5ab3
Eben Burger <eben@arlowconsulting.co.za>
2021-05-05 12:40:09 +0200

    removed layout and nav components. proj update

---
+++ Disney-plus-clone.WebClient/Disney-plus-clone.WebClient/ClientApp/src/components/login/login.js
@@ -0,0 +1,12 @@
+import React, {} from 'react';
+
+const Login = (props) => {
+    return (
+        <>
+            <h1>
+                Hello
+            </h1>
+        </>
+    );
+}
+export default Login;
