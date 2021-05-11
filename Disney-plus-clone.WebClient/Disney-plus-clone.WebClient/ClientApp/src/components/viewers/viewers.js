3e011b24416d9e2d910f537af8963d855adb87b6
Eben Burger <eben@arlowconsulting.co.za>
2021-05-11 12:40:01 +0200

    added videos to viewer boxes

--- Disney-plus-clone.WebClient/Disney-plus-clone.WebClient/ClientApp/src/components/viewers/viewers.js
+++ Disney-plus-clone.WebClient/Disney-plus-clone.WebClient/ClientApp/src/components/viewers/viewers.js
@@ -7,18 +7,33 @@ const Viewers = (props) => {
             <Container>
                 <Wrap>
                     <img src='/images/viewers-disney.png' alt='Disney viewer' />
+                    <video autoPlay={true} loop={true} playsInline={true} >
+                        <source src='/videos/1564674844-disney.mp4' type='video/mp4'/>
+                    </video>
                 </Wrap>
                 <Wrap>
                     <img src='/images/viewers-marvel.png' alt='Marvel viewer' />
+                    <video autoPlay={true} loop={true} playsInline={true} >
+                        <source src='/videos/1564676115-marvel.mp4' type='video/mp4'/>
+                    </video>
                 </Wrap>
                 <Wrap>
                     <img src='/images/viewers-national.png' alt='National viewer' />
+                    <video autoPlay={true} loop={true} playsInline={true} >
+                        <source src='/videos/1564676296-national-geographic.mp4' type='video/mp4'/>
+                    </video>
                 </Wrap>
                 <Wrap>
                     <img src='/images/viewers-pixar.png' alt='Pixar viewer' />
+                    <video autoPlay={true} loop={true} playsInline={true} >
+                        <source src='/videos/1564676714-pixar.mp4' type='video/mp4'/>
+                    </video>
                 </Wrap>
                 <Wrap>
                     <img src='/images/viewers-starwars.png' alt='Star Wars viewer' />
+                    <video autoPlay={true} loop={true} playsInline={true} >
+                        <source src='/videos/1608229455-star-wars.mp4' type='video/mp4'/>
+                    </video>
                 </Wrap>
             </Container>
         </>
@@ -60,6 +75,26 @@ const Wrap = styled.div`
         transition: opacity 500ms ease-in-out 0s;
         width: 100%;
         z-index: 1;
+    }
+    
+    video {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        top: 0;
+        opacity: 0;
+        z-index: 0;
+    }
+    
+    &:hover {
+        box-shadow: rgb(0 0 0 / 80%) 0 40px 58px -16px,
+                    rgb(0 0 0 / 72%) 0 30px 22px -10px;
+        transform: scale(1.05);
+        border-color: rgba(49, 249, 249, 0.8);
+        
+        video {
+            opacity: 1;
+        }
     }
 `;
 
