afd5212d74c7257353c20123ad1fc5f3623a2690
Eben Burger <eben@arlowconsulting.co.za>
2021-05-12 15:09:15 +0200

    added image title to detail page

--- Disney-plus-clone.WebClient/Disney-plus-clone.WebClient/ClientApp/src/components/detail/detail.js
+++ Disney-plus-clone.WebClient/Disney-plus-clone.WebClient/ClientApp/src/components/detail/detail.js
@@ -9,6 +9,17 @@ const Detail = (props) => {
                     <img src='https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/D53D1F5D357587A8D09067AB09FFC7096F837CBAAE02BDC3C0E75814471A1E36/scale?width=1440&aspectRatio=1.78&format=jpeg'
                          alt='img'/>
                 </Background>
+                
+                <ImageTitle>
+                    <img src='https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/47A6FB38D95B3A5EF5583C9EED0B698ED2992CBA4AC7222DD3269DC92DFA03A6/scale?width=1440&aspectRatio=1.78'
+                         alt='image'/>
+                </ImageTitle>
+                
+                <ContentMeta>
+                    <Controls>
+                        controls
+                    </Controls>
+                </ContentMeta>
             </Container>
         </>
     );
@@ -39,6 +50,35 @@ const Background = styled.div`
             width: initial;
         }
     }
-    
 `;
+
+const ImageTitle = styled.div`
+    align-item: flex-end;
+    display: flex;
+    justify-content: flex-start;
+    margin: 0 auto;
+    height: 30vw;
+    min-height: 170px;
+    padding-bottom: 24px;
+    width: 100%;
+    
+    img {
+        max-width: 600px;
+        min-width: 200px;
+        width: 35vw;
+    }
+`;
+
+const ContentMeta = styled.div`
+    max-with: 874px;
+`;
+
+const Controls = styled.div`
+    display: flex;
+    align-items: center;
+    flex-flow: row nowrap;
+    margin: 24px 0;
+    min-height: 56px;
+`;
+
 export default Detail;
