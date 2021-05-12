38764cda25adc3ca3fec49e51bae857b4c2e6bc1
Eben Burger <eben@arlowconsulting.co.za>
2021-05-12 14:56:45 +0200

    added details page with background image

--- Disney-plus-clone.WebClient/Disney-plus-clone.WebClient/ClientApp/src/App.js
+++ Disney-plus-clone.WebClient/Disney-plus-clone.WebClient/ClientApp/src/App.js
@@ -7,6 +7,7 @@ import Home from "./components/home/home";
 import './custom.xs.css'
 import './custom.sm.css'
 import './custom.md.css'
+import Detail from "./components/detail/detail";
 
 
 const App = () => {
@@ -14,15 +15,18 @@ const App = () => {
       <div className='app'>
         <Router>
             <Header />
-          <Switch>
-            <Route path="/home">
-                <Home/>
-            </Route>              
-            <Route exact path='/'>
-                <Login/>
-            </Route>
-              
-          </Switch>
+            <Switch>
+                <Route exact path='/'>
+                    <Login/>
+                </Route>
+                <Route path="/home">
+                    <Home/>
+                </Route>
+                <Route path='/detail/:id'>
+                    {/*this path will have "detail" followed by the "id" of the movie*/}
+                    <Detail />
+                </Route>
+            </Switch>
         </Router>
       </div>
   )
